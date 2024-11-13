e883eb78ba30140b25e3c28970c7054a8dd2c955
unknown <rafagonpin317@gmail.com>
2024-11-13 10:02:58 -0300

    Registro pronto

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.example.model.Aluno;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+        Aluno aluno1 = new Aluno("1232434", "José", "12/03/2014", 70, 1.71);
+        aluno1.calculaIMC();
     }
 }
--- src/main/java/org/example/model/Aluno.java
+++ src/main/java/org/example/model/Aluno.java
@@ -1,6 +1,11 @@
 package org.example.model;
 
-import java.util.Date;
+import javax.swing.text.DateFormatter;
+import java.io.*;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Aluno {
     private String cpf;
@@ -58,4 +63,76 @@ public class Aluno {
         this.altura = altura;
     }
 
+    public void calculaIMC() throws IOException {
+        DecimalFormat df = new DecimalFormat("#.#");
+        Double imc = peso / Math.pow(altura, 2);
+        String imcString = df.format(imc);
+
+        //Chama o método que define a condição do aluno
+        String condicaoAluno = defineIMC(imc);
+
+        //Registra as informações
+        registraIMC(imcString, condicaoAluno);
+    }
+
+    public String defineIMC(double imc){
+        String condicao = "";
+        if(imc <= 18.5){
+            condicao = "Abaixo do Peso";
+        }
+        else if (imc <= 24.9) {
+            condicao = "Normar";
+        }
+        else if (imc <= 29.9) {
+            condicao = "Sobrepeso";
+        }
+        else if (imc <= 34.9) {
+            condicao = "Obesidade I";
+        }
+        else if (imc <= 39.9) {
+            condicao = "Obesidade II";
+        }
+        else if (imc > 40) {
+            condicao = "Obesidade III";
+        }
+
+        return condicao;
+    }
+
+    public void registraIMC(String imc, String condicao) throws IOException {
+
+        //Pega a data em que o código é executado e formata
+        LocalDate dataAtual = LocalDate.now();
+        DateTimeFormatter dataFormatacao = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        String dataRegistro = dataAtual.format(dataFormatacao);
+
+        //INICIANDO O ARQUIVO
+        //Verifica se o diretório existe
+        File diretorio = new File("registros");
+        if(!diretorio.exists()){
+            diretorio.mkdirs();
+            System.out.println("Criando o Diretório...");
+        }
+
+        //Verifica se o arquivo
+        File registro = new File(diretorio, "registro-"+cpf+".txt");
+        if(!registro.exists()){
+            registro.createNewFile();
+            System.out.println("Criando o Registro...");
+        }
+
+        //Escrevendo o registro no arquivo
+        FileWriter fw = new FileWriter(registro, true);
+        BufferedWriter escritor = new BufferedWriter(fw);
+
+        System.out.println("Escrevendo o Registro...");
+
+        escritor.write("Nome: " + nome + " | CPF: " + cpf + " | Data do Registro: [ "+ dataRegistro +" ]");
+        escritor.newLine();
+        escritor.write( "O IMC calculado foi "+ imc + " sua situação é " + condicao +"!");
+        escritor.newLine();
+        escritor.newLine();
+        escritor.close();
+    }
 }
